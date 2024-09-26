0aeb5caed1bb4790a1d6bb059c835adec9e20fc1
GitUser1233-afk <guptaabhinav2408@gmail.com>
2024-09-26 21:15:49 +0530

    hi

---
+++ passwordChecker.py
@@ -0,0 +1,5 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+print("Hello, " + username)
+passLen = len(password)
+print("Your Password, " + '*'*passLen + ", is " + str(passLen) + " characters long.")
